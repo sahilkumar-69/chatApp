474b07980adf624def2bb00b99b0d22e4e9a7da5
Sahil <sahilkumar6939@gmail.com>
2025-09-15 22:08:52 +0530

    frontend complete

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,36 @@
 import React, { useState } from "react";
+import { HiOutlineChatBubbleLeftRight } from "react-icons/hi2";
+import {  useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
+  const navigate = useNavigate();
   const [selectedImage, setSelectedImage] = useState("");
+  const [FormData, setFormData] = useState({
+    name: "",
+    bio: "",
+  });
+
+  const handleOnChange = (e) => {
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+  };
 
   return (
     <div className="min-h-screen bg-cover bg-no-repeat flex items-center justify-center">
       <div className="w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg ">
-        <form className="flex flex-col gap-5 p-10 flex-1">
+        <form
+          onSubmit={handleOnSubmit}
+          className="flex flex-col gap-5 p-10 flex-1"
+        >
           <h3 className="text-lg">Profile details</h3>
           <label
             htmlFor="avatar"
@@ -33,7 +57,37 @@ const ProfilePage = () => {
             />
             Upload profile image
           </label>
+          <input
+            type="text"
+            value={FormData.name}
+            onChange={handleOnChange}
+            name="name"
+            placeholder="Your name"
+            required
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            id=""
+          />
+          <textarea
+            name="bio"
+            required
+            value={FormData.bio}
+            onChange={handleOnChange}
+            placeholder="Write profile bio"
+            className="p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500"
+            id=""
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer"
+          >
+            Save
+          </button>
         </form>
+        <HiOutlineChatBubbleLeftRight
+          className="max-w-44 aspect-square rounded-full max-sm:mt-10 mx-10"
+          color="white"
+          size={400}
+        />
       </div>
     </div>
   );
